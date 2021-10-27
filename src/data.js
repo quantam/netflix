73267257685b7e14ac9df7562280394fbddd9a52
Santu Rana <santurana.89@gmail.com>
2021-10-27 07:21:52 +0530

    class 2

---
+++ src/data.js
@@ -0,0 +1,38 @@
+const data = [
+  {
+    name: 'Money Heist',
+    url: 'https://www.netflix.com/in/title/80192098',
+    imageUrl: 'https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Money-Heist.jpg',
+    description:
+      'The Netflix series Money Heist (La casa de Papel) is a Spanish heist crime drama television series created by Álex Pina for Netflix. It is rated TV-MA for Violence, Sex, Nudity, Substances, Sexual Violence.',
+  },
+  {
+    name: 'Stranger Things',
+    url: 'https://www.netflix.com/in/title/80192098',
+    imageUrl: 'https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Money-Heist.jpg',
+    description:
+      'The Netflix series Money Heist (La casa de Papel) is a Spanish heist crime drama television series created by Álex Pina for Netflix. It is rated TV-MA for Violence, Sex, Nudity, Substances, Sexual Violence.',
+  },
+  {
+    name: 'Squid Game',
+    url: 'https://www.netflix.com/in/title/80192098',
+    imageUrl: 'https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Squid-Game.jpg',
+    description:
+      'The Netflix series Squid Game (Round Six) is an upcoming South Korean television series written and directed by Hwang Dong-hyuk. It tells the story of people who risk their lives in a mysterious survival game having a prize of $45.6 billion. It is rated TV-MA for violence, sex nudity, suicide.',
+  },
+  {
+    name: 'Breaking Bad',
+    url: 'https://www.netflix.com/in/title/80192098',
+    imageUrl: 'https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Breaking-Bad.jpg',
+    description:
+      'The Netflix series Money Heist (La casa de Papel) is a Spanish heist crime drama television series created by Álex Pina for Netflix. It is rated TV-MA for Violence, Sex, Nudity, Substances, Sexual Violence.',
+  },
+  {
+    name: 'Lucifer',
+    url: 'https://www.netflix.com/in/title/8019209',
+    imageUrl: 'https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Lucifer.jpg',
+    description: 'The Netflix series Lucifer, an American urban fantasy television series created by Tom Kapinos for Netflix. It is rated TV-14  for violence, sexual content, nudity, and drug usage.',
+  },
+];
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card';
+import data from './data';
 
 ReactDOM.render(
   <>
+    <h1 className="header">Top netflix series list</h1>
     <div className="d-flex justify-content-start cards">
-      <Card
-        imgsrc="https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Money-Heist.jpg"
-        title="Money Heist"
-        linkUrl="https://www.netflix.com/in/title/80192098"
-        description="
-    The Netflix series Money Heist (La casa de Papel) is a Spanish heist crime drama television series created by Álex Pina for Netflix. It is rated TV-MA for Violence, Sex, Nudity,
-    Substances, Sexual Violence."
-      />
-      <Card
-        imgsrc="https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Squid-Game.jpg"
-        title="Squid Game"
-        linkUrl="https://www.netflix.com/in/title/80192098"
-        description="
-    The Netflix series Money Heist (La casa de Papel) is a Spanish heist crime drama television series created by Álex Pina for Netflix. It is rated TV-MA for Violence, Sex, Nudity,
-    Substances, Sexual Violence."
-      />
-      <Card
-        imgsrc="https://cdn.statically.io/img/www.ageratingjuju.com/f=auto%2Cq=60/wp-content/uploads/2021/10/Breaking-Bad.jpg"
-        title="Breaking Bad"
-        linkUrl="https://www.netflix.com/in/title/80192098"
-        description="
-    The Netflix series Money Heist (La casa de Papel) is a Spanish heist crime drama television series created by Álex Pina for Netflix. It is rated TV-MA for Violence, Sex, Nudity,
-    Substances, Sexual Violence."
-      />
+      {data.map((series) => {
+        return <Card imgsrc={series.imageUrl} title={series.name} linkUrl={series.url} description={series.description} />;
+      })}
     </div>
   </>,
   document.getElementById('root')
